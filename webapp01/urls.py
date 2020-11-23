b54ed95852e639a7e6c76fb519a76534f3ec3b0c
RamgopalYalla <ramgopal678@gmail.com>
2020-11-23 19:18:40 +0100

    My first website

--- webapp01/urls.py
+++ webapp01/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('page1/', views.index2, name="page1"),
+    path("<int:id>", views.index, name="index"),
+
 ]
--- webapp01/views.py
+++ webapp01/views.py
@@ -3,8 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1> My website page 1</h1>")
+def index(response, id):
+    return HttpResponse("<h1>%s</h1>" % id)
 
-def index2(response):
-    return HttpResponse("<h1> My website page 2</h1>")
+
