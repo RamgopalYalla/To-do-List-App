fcbc9293dd114944175a20f49e986d84eb2dad02
RamgopalYalla <ramgopal678@gmail.com>
2020-11-26 12:47:28 +0100

    My first website

--- webapp01/views.py
+++ webapp01/views.py
@@ -1,12 +1,51 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDolist, Item
+from .forms import CreateNewList
 
 
 def index(response, id):
     ls = ToDolist.objects.get(id=id)
-    return render(response, "webapp01/list.html", {"ls":ls})
+
+    if response.method == "POST":
+        print(response.POST)
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+                item.save()
+
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+
+            if len(txt) > 2:
+                ls.item_set.create(text=txt, complete=False)
+            else:
+                print("invalid")
+
+
+
+    return render(response, "webapp01/list.html", {"ls": ls})
 
 
 def home(response):
-    return render(response, "webapp01/home.html", {"name": "test"})
+    return render(response, "webapp01/home.html", {})
+
+
+def create(response):
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDolist(name=n)
+            t.save()
+
+        return HttpResponseRedirect("/%i" %t.id)
+
+    else:
+        form = CreateNewList()
+
+    return render(response, "webapp01/create.html", {"form": form})
